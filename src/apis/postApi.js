13d2800c8423ea2755d0d36b9f22acdacbe11cba
kim min seok <nas7062@naver.com>
2025-05-23 17:23:31 +0900

    Feat : 글 작성 및 글 조회기능 추가

--- src/apis/postApi.js
+++ src/apis/postApi.js
@@ -2,7 +2,14 @@ import axios from 'axios'
 axios.defaults.withCredentials = true
 const API_URL = import.meta.env.VITE_API_URL
 
-export const createPost = data => {
-  const response = axios.post(`${API_URL}/createpost`, data)
+export const createPost = async data => {
+  const response = await axios.post(`${API_URL}/createpost`, data)
+  return response.data
+}
+
+export const getAllPost = async (page = 0, limit = 3) => {
+  const response = await axios.get(`${API_URL}/postlist`, {
+    params: { page, limit },
+  })
   return response.data
 }
